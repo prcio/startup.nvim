1a2186967a28424033f630af2c8db58d47721334
Dennis Garcia <160544335+prcio@users.noreply.github.com>
2024-10-27 22:28:02 -0400

    change some quotes

--- lua/startup/quotes.lua
+++ lua/startup/quotes.lua
@@ -10,12 +10,9 @@ local quotes = {
         "- Alan Kay",
     },
     {
-        "Always code as if the person who ends up maintaining your code is a violent psychopath who knows where you live.",
-        "",
-        "- John Woods",
-    },
-    {
         "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.",
+        "",
+        "- Antoine de Saint-Exupéry",
     },
     {
         "If you don't make mistakes, you're not working on hard enough problems.",
@@ -23,85 +20,94 @@ local quotes = {
         "- Frank Wilczek",
     },
     {
-        "Use tracer bullets to find the target.",
-        "",
-        "Tracer bullets let you home in on your target by trying things and seeing how close they land.",
-    },
-    {
-        "Always design for concurrency.",
-        "",
-        "Allow for concurrency, and you'll design cleaner interfaces with fewer assumptions.",
-    },
-    {
-        "Test your software, or your users will.",
-        "",
-        "Test ruthlessly. Don't make your users find bugs for you.",
-    },
-    {
-        "Don't live with broken windows.",
-        "",
-        "Fix bad designs, wrong decisions, and poor code when you see them.",
-    },
-    {
-        "Keep knowledge in plain text.",
-        "",
-        "Plain text won't become obsolete. It helps leverage your work and simplifies debugging and testing.",
-    },
-    {
-        "Use a single editor well.",
-        "",
-        "The editor should be an extension of your hand; make sure your editor is configurable, extensible, and programmable.",
-    },
-    {
-        "Crash early.",
-        "",
-        "A dead program normally does a lot less damage than a crippled one.",
-    },
-    {
-        "Design to test.",
-        "",
-        "Start thinking about testing before you write a line of code.",
-    },
-    {
-        "Work with a user to think like a user.",
-        "",
-        "It's the best way to gain insight into how the system will really be used.",
-    },
-    {
-        "Test early. Test often. Test automatically.",
-        "",
-        "Tests that run with every build are much more effective than test plans that sit on a shelf.",
-    },
-    {
-        "Use saboteurs to test your testing.",
-        "",
-        "Introduce bugs on purpose in a separate copy of the source to verify that testing will catch them.",
-    },
-    {
-        "There's an old story about the person who wished his computer were as easy to use as his telephone. That wish has come true, since I no longer know how to use my telephone.",
-        "",
-        "- Bjarne Stroustrup",
-    },
-    {
-        'There are only two industries that refer to their customers as "users".',
-        "",
-        "- Edward Tufte",
-    },
-    {
         "Easy things should be easy and hard things should be possible.",
         "",
         "- Larry Wall",
     },
-    { "They did not know it was impossible, so they did it!", "", "- Mark Twain" },
     {
-        "If debugging is the process of removing bugs, then programming must be the process of putting them in.",
+        "The first principle is that you must not fool yourself, and you are the easiest person to fool.",
         "",
-        "- Edsger W. Dijkstra",
+        "- Richard Feynman",
     },
     {
-        "The average user doesn't give a damn what happens, as long as (1) it works and (2) it's fast.",
+        "What we observe is not nature itself, but nature exposed to our method of questioning.",
         "",
-        "- Daniel J. Bernstein",
+        "- Werner Heisenberg",
+    },
+    {
+        "Mathematics is the language in which God has written the universe.",
+        "",
+        "- Galileo Galilei",
+    },
+    {
+        "The universe is under no obligation to make sense to you.",
+        "",
+        "- Neil deGrasse Tyson",
+    },
+    {
+        "Pure mathematics is, in its way, the poetry of logical ideas.",
+        "",
+        "- Albert Einstein",
+    },
+    {
+        "In science, we must be interested in things, not in persons.",
+        "",
+        "- Marie Curie",
+    },
+    {
+        "Not only is the universe stranger than we think, it is stranger than we can think.",
+        "",
+        "- Werner Heisenberg",
+    },
+    {
+        "I seem to have been only like a boy playing on the seashore... finding a smoother pebble or a prettier shell... while the great ocean of truth lay all undiscovered before me.",
+        "",
+        "- Isaac Newton",
+    },
+    {
+        "Those who are not shocked when they first come across quantum theory cannot possibly have understood it.",
+        "",
+        "- Niels Bohr",
+    },
+    {
+        "Philosophy is written in this grand book, the universe, which stands continually open to our gaze.",
+        "",
+        "- Galileo Galilei",
+    },
+    {
+        "There is no law except the law that there is no law.",
+        "",
+        "- John Wheeler",
+    },
+    {
+        "No one can escape the influence of a prevailing ideology.",
+        "",
+        "- Ludwig Boltzmann",
+    },
+    {
+        "Mathematics knows no races or geographic boundaries; for mathematics, the cultural world is one country.",
+        "",
+        "- David Hilbert",
+    },
+    {
+        "The true sign of intelligence is not knowledge but imagination.",
+        "",
+        "- Albert Einstein",
+    },
+    {
+        "The scientist does not study nature because it is useful; he studies it because he delights in it, and he delights in it because it is beautiful.",
+        "",
+        "- Henri Poincaré",
+    },
+    {
+        "Science is the poetry of reality.",
+        "",
+        "- Richard Dawkins",
+    },
+    {
+        "Life is not only stranger than we imagine, it is stranger than we can imagine.",
+        "",
+        "- J.B.S. Haldane",
     },
 }
 
